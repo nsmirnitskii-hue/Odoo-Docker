cdbde69e52d3e68613412e6b9d53db0be735f954
nsmirnitskii-hue <n.smirnitskii@ikasle.eus>
2026-01-26 21:26:33 +0100

    Odoo echo

--- addons/didaktikapp/models/horas.py
+++ addons/didaktikapp/models/horas.py
@@ -6,29 +6,29 @@ class DidaktikAppHoras(models.Model):
     _description = 'Horas trabajadas por empleado'
 
 
-employee_id = fields.Many2one(
-'hr.employee',
-string="Empleado",
-default=lambda self: self.env.user.employee_id,
-readonly=True
-)
+    employee_id = fields.Many2one(
+        'hr.employee',
+        string="Empleado",
+        default=lambda self: self.env.user.employee_id,
+        readonly=True
+    )
 
 
-app_id = fields.Many2one(
-'didaktikapp.app',
-string="Aplicación",
-required=True
-)
+    app_id = fields.Many2one(
+        'didaktikapp.app',
+        string="Aplicacion",
+        required=True
+    )
 
 
-fecha = fields.Date(
-string="Fecha",
-default=fields.Date.today,
-required=True
-)
+    fecha = fields.Date(
+        string="Fecha",
+        default=fields.Date.today,
+        required=True
+    )
 
 
-horas = fields.Float(
-string="Horas trabajadas",
-required=True
-)
+    horas = fields.Float(
+        string="Horas trabajadas",
+        required=True
+    )
